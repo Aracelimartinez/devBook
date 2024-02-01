4a487d1cbf6811c836e5b8bda50defbec722abe7
Araceli Martinez <araceli_martinez94@hotmail.com>
2024-02-01 16:32:52 -0300

    correct form style

--- webapp/src/views/LogInView.vue
+++ webapp/src/views/LogInView.vue
@@ -8,15 +8,21 @@
       <div class="m-5 p-5 w-10/12">
         <img alt="LogIn image"  src="@/assets/undraw_right_places_re_3sve.svg"/>
       </div>
-      <div class="w-10/12 p-4 flex flex-col text-base signin-card">
-        <h1 class="my-3 text-xl font-bold">Iniciar sessão</h1>
-        <input class="my-2 p-2 border rounded-md border-color-secondary hover:border-color-primary duration-150 fontAwesome" type="email" placeholder="&#xf0e0;  E-mail" >
-        <input class="my-2 p-2 border rounded-md border-color-secondary hover:border-color-primary duration-150 fontAwesome" type="password" placeholder="&#xf084;  Senha" >
-        <button class="mt-2 py-2 px-6  text-white bg-color-primary rounded-md hover:opacity-80 duration-150">Log in</button>
-        <div class="my-2 flex justify-center text-line">
-          <span>ou</span>
+      <div class="w-10/12 flex flex-col justify-center">
+        <h1 class="mb-5 text-3xl font-bold text-center">Seja bem vindo ao <span class="text-color-primary">DevBook</span></h1>
+        <!-- Sign in card -->
+        <div class=" flex flex-col p-4 text-base signin-card">
+          <h2 class="my-2 text-xl font-bold">Iniciar sessão</h2>
+          <form class="flex flex-col" action="" name="login-fom">
+            <input class="my-2 p-2 border rounded-md border-color-secondary hover:border-color-primary duration-150 fontAwesome" type="email" placeholder="&#xf0e0;  E-mail" required="required">
+            <input class="my-2 p-2 border rounded-md border-color-secondary hover:border-color-primary duration-150 fontAwesome" type="password" placeholder="&#xf084;  Senha" required="required">
+            <button type="submit" class="mt-2 py-2 px-6  text-white bg-color-primary rounded-md hover:opacity-80 duration-150">Log in</button>
+          </form>
+          <div class="my-2 text-line">
+            <span>ou</span>
+          </div>
+          <button class="mb-3 py-2 px-6 bg-color-secondary rounded-md hover:opacity-80 duration-150">Criar conta</button>
         </div>
-        <button class="mb-3 py-2 px-6 bg-color-secondary rounded-md hover:opacity-80 duration-150">Criar conta</button>
       </div>
     </div>
   </main>
@@ -34,6 +40,7 @@
     background-color: #fff;
     box-shadow: 0px 0px 2px 2px rgba(0, 0, 0, 0.05);
     border-radius: 8px;
+    max-width: 600px;
   }
 
   .text-line {
@@ -58,7 +65,7 @@
   .fontAwesome {
   font-family: 'Roboto', FontAwesome, sans-serif;
   font-weight: 100;
-}
+  }
 
   // Smallest device
   @media (min-width: 100px) and (max-width: 575px) {
@@ -78,6 +85,7 @@
   @media (min-width: 768px) {
     #login-page {
       grid-template-columns: 1fr 1fr;
+      margin: 0px 80px;
     }
   }
 </style>
